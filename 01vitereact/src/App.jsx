0a76285b394af221bfed7c24a334d03748057327
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2024-03-26 12:11:27 +0530

    Adds some custom funcitons , Objects, and elements to create own elements inside the React.

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,13 +1,15 @@
 import Chai from "./Chai"
 
 function App() {
- 
+ const username = "Dinesh Sutihar"
+
 
   return (
     // This is a fragment
     <> 
     <Chai />
-    <h1>Hello form vite basic | Dinesh Sutihar</h1>
+    {/* This is a JSX expression we cannot user any other codes like if for inside the {} */}
+    <h1>Hello form vite basic | {username}</h1>  
     </>
   )
 }
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,43 @@
+// eslint-disable-next-line no-unused-vars
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+// const reactElement = {
+//   type: 'a',
+//   props: {
+//       href: 'https://www.google.com',
+//       target: '_blank',
+//   },
+//   children: 'Click me to visit Google'
+// }
+
+
+// const anotherElement = (
+//     <div>
+//         <a href='https://www.google.com' target='_blank'>Click me to visit Google</a>
+//     </div>
+
+// )
+
+
+const anotherUser = "Umesh Sutihar"
+
+
+const reactElement = React.createElement(
+  'a', 
+  {
+    href: "https://www.google.com",
+    target: "_blank"
+  },
+  'Click me to visit Google | ',
+  anotherUser
+)
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+<>
+  {reactElement}
+   <App />
+</>
 )
