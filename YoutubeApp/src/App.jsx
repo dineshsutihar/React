6cfb7f475bc18b920460996e822058f1f66374a3
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2024-06-13 11:59:01 +0530

    Adds: complete setup of firebase React App.

--- YoutubeApp/src/App.jsx
+++ YoutubeApp/src/App.jsx
@@ -1,10 +1,30 @@
+import { getDatabase, ref, set } from "firebase/database";
+import { app } from "./firebase";
+
+const db = getDatabase(app);
+
 
 function App() {
 
+  const puData  = () =>{
+    // set(ref(db, "users/dinesh"),{
+    //   id:1, 
+    //   name: "Dinesh",
+    //   age: 22
+    // })
+    set(ref(db, "users/akash"),{
+      id:2, 
+      name: "Akash",
+      age: 22
+    })
+  }
 
   return (
     <>
+    <div >
      <h1>Firebase React App</h1>
+     <button onClick={puData}>Put Data</button>
+     </div>
     </>
   )
 }
