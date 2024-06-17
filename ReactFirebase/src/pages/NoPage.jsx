4710420df801ff606173ca06b769302e4977372c
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2024-06-17 22:25:55 +0530

    Fixes some error in Nopage component which is the height of page

--- ReactFirebase/src/pages/NoPage.jsx
+++ ReactFirebase/src/pages/NoPage.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const NoPage = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white">
+    <div className="flex flex-col items-center justify-center  bg-gray-900 min-h-[85vh] text-white">
       <h1 className="text-4xl font-bold mb-4">404</h1>
       <p className="text-xl mb-4">Page Not Found</p>
       <Link to="/" className="bg-gray-700 text-white py-2 px-4 rounded hover:bg-gray-600">
