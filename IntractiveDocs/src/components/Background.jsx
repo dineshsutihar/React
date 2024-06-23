b6677fbdebb062c9a656c91299ea99dbf06eb044
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2024-06-23 16:06:01 +0530

    refactor - some code is added and some are removed

--- IntractiveDocs/src/components/Background.jsx
+++ IntractiveDocs/src/components/Background.jsx
@@ -2,10 +2,10 @@
 function Background() {
   return (
     <div className="fixed z-[2] h-screen w-full">
-      <div className="top-[5%] absolute w-full py-10 flex justify-center text-zinc-600 text-xl font-semibold">
+      <div className="top-[5%] absolute w-full py-10 flex justify-center text-zinc-600 text-2xl font-semibold">
         Documents.
       </div>
-      <h1 className="absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]  text-[13vw] leading-none tracking-tighter font-semibold text-zinc-900 ">
+      <h1 className="absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]  text-[20vw] leading-none tracking-tighter font-semibold text-zinc-900 ">
         Docs.
       </h1>
     </div>
--- IntractiveDocs/src/components/Foreground.jsx
+++ IntractiveDocs/src/components/Foreground.jsx
@@ -1,48 +1,80 @@
 import { useRef } from "react";
-import Card from "./Card"
-
+import Card from "./Card";
 
 function Foreground() {
-    const reference = useRef(null);
-    const data =[
-        {
-            desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-            filesize: ".9mb",
-            close: true,
-            tag:{
-                isOpen:true, 
-                tagTitle:"Download Now",
-                tagColor:"green"
-            }
-        },
-        {
-            desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-            filesize: ".9mb",
-            close: true,
-            tag:{
-                isOpen:true, 
-                tagTitle:"Download Now",
-                tagColor:"blue"
-            }
-        },
-        {
-            desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-            filesize: ".9mb",
-            close: true,
-            tag:{
-                isOpen:false, 
-                tagTitle:"Download Now",
-                tagColor:"green"
-            }
-        },
-    ]
+  const reference = useRef(null);
+  const data = [
+    {
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      filesize: ".9mb",
+      close: true,
+      tag: {
+        isOpen: true,
+        tagTitle: "Download Now",
+        tagColor: "green",
+      },
+    },
+    {
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      filesize: ".9mb",
+      close: true,
+      tag: {
+        isOpen: true,
+        tagTitle: "Download Now",
+        tagColor: "blue",
+      },
+    },
+    {
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      filesize: ".9mb",
+      close: true,
+      tag: {
+        isOpen: false,
+        tagTitle: "Download Now",
+        tagColor: "green",
+      },
+    },
+    {
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      filesize: "100mb",
+      close: false,
+      tag: {
+        isOpen: false,
+        tagTitle: "Download Now",
+        tagColor: "green",
+      },
+    },
+    {
+      desc: "This is just a test to test components of javascript.",
+      filesize: ".9mb",
+      close: false,
+      tag: {
+        isOpen: true,
+        tagTitle: "Submit Now",
+        tagColor: "green",
+      },
+    },
+    {
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      filesize: ".9mb",
+      close: true,
+      tag: {
+        isOpen: true,
+        tagTitle: "Upload Now",
+        tagColor: "blue",
+      },
+    },
+  ];
   return (
-    <div ref={reference} className='fixed top-0 left-0 z-[3] w-full h-full flex gap-10 flex-wrap p-5'>
-        {data.map((item, index)=>(
-            <Card data={item} key={index} refer={reference} />
-        ))}
+    <div
+      ref={reference}
+      className="fixed top-0 left-0 z-[3] w-full h-full flex gap-10 flex-wrap p-5"
+    >
+      {data.map((item, index) => (
+        <Card data={item} key={index} refer={reference} />
+      ))}
     </div>
-  )
+  );
 }
 
-export default Foreground
+export default Foreground;
