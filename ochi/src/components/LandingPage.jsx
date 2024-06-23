7182ccc207c9d167b505de5774382395517d6bf2
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2024-06-24 02:30:36 +0530

    refactor- adds 2 fonts and changes tsconfig file to add those 2 fonts in it

---
+++ ochi/src/components/LandingPage.jsx
@@ -0,0 +1,13 @@
+export default function LandingPage() {
+  return (
+    <div className="w-full h-screen bg-zinc-900">
+      <div className="textstructure">
+        <div className="masker ">
+          <h1 className=" uppercase text-9xl font-FounderGrotesk">
+            We Create
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+}
