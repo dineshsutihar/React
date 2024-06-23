6eff74d561c05962d69f8b522a00cc56eb7ef10e
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2024-06-24 02:06:05 +0530

    feat- add navigation bar on page

--- ochi/src/App.jsx
+++ ochi/src/App.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <div className="w-full h-screen bg-zinc-900 text-white">
+        <Navbar />
+      </div>;
+    </>
+  );
 }
 
 export default App;
